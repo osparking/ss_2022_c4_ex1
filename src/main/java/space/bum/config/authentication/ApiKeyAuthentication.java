517de0a3220707a718c8a7b74c91287a9378ba38
osparking <jbpark03@gmail.com>
2023-08-14 22:16:37 +0900

    api key authen class skeleton

---
+++ src/main/java/space/bum/config/authentication/ApiKeyAuthentication.java
@@ -0,0 +1,53 @@
+package space.bum.config.authentication;
+
+import java.util.Collection;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+
+public class ApiKeyAuthentication implements Authentication{
+
+	@Override
+	public boolean isAuthenticated() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void setAuthenticated(boolean isAuthenticated)
+			throws IllegalArgumentException {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	@Override
+	public String getName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object getCredentials() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object getDetails() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object getPrincipal() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
