5b472d7703f6fadb1f4abb5ca2b64811de645c94
osparking <jbpark03@gmail.com>
2023-08-14 22:08:54 +0900

    ApiKeyFilter uses key property

--- src/main/java/space/bum/config/filters/ApiKeyFilter.java
+++ src/main/java/space/bum/config/filters/ApiKeyFilter.java
@@ -7,16 +7,23 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.filter.OncePerRequestFilter;
 
+import lombok.AllArgsConstructor;
+
+// 이것을 어디에도 두고 싶진 않다.
+@AllArgsConstructor
 public class ApiKeyFilter extends OncePerRequestFilter{
 
+	@Value("${the.key}")
+	private final String key;
+	
 	@Override
 	protected void doFilterInternal(HttpServletRequest request,
 			HttpServletResponse response, FilterChain filterChain)
 			throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		
+		filterChain.doFilter(request, response); // 언젠가 호출해야...
 	}
 
 }
