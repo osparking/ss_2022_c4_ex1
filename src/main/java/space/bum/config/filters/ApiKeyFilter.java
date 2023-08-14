969dbddd28b4fc6434837fda56fbbb6ad689fa1a
osparking <jbpark03@gmail.com>
2023-08-14 17:40:21 +0900

    API Key filter class structure

---
+++ src/main/java/space/bum/config/filters/ApiKeyFilter.java
@@ -0,0 +1,22 @@
+package space.bum.config.filters;
+
+import java.io.IOException;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.filter.OncePerRequestFilter;
+
+public class ApiKeyFilter extends OncePerRequestFilter{
+
+	@Override
+	protected void doFilterInternal(HttpServletRequest request,
+			HttpServletResponse response, FilterChain filterChain)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
