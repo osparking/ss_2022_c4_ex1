99ef5c267176e652474a712a033105beb97acba0
osparking <jbpark03@gmail.com>
2023-08-15 15:15:01 +0900

    custom authen manager skeleton

---
+++ src/main/java/space/bum/config/managers/CustomAuthenticationManager.java
@@ -0,0 +1,16 @@
+package space.bum.config.managers;
+
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+
+public class CustomAuthenticationManager implements AuthenticationManager{
+
+	@Override
+	public Authentication authenticate(Authentication authentication)
+			throws AuthenticationException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
