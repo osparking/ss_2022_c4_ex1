3fb401825229df869e7efa565f3be2674c3abb48
osparking <jbpark03@gmail.com>
2023-08-15 15:56:25 +0900

    custom manager uses custom provider

--- src/main/java/space/bum/config/managers/CustomAuthenticationManager.java
+++ src/main/java/space/bum/config/managers/CustomAuthenticationManager.java
@@ -4,13 +4,23 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 
+import lombok.AllArgsConstructor;
+import space.bum.config.providers.ApiKeyProvider;
+
+@AllArgsConstructor
 public class CustomAuthenticationManager implements AuthenticationManager{
 
+	private final String key;
+	
 	@Override
 	public Authentication authenticate(Authentication authentication)
 			throws AuthenticationException {
-		// TODO Auto-generated method stub
-		return null;
+		var provider = new ApiKeyProvider(key);
+		
+		if (provider.supports(authentication.getClass())) {
+			return provider.authenticate(authentication);
+		}
+		return authentication;
 	}
 
 }
