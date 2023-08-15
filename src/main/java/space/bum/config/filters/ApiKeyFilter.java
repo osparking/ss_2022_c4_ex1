edd2e77ea1f6a4e3b0431032ce649298d16efb24
osparking <jbpark03@gmail.com>
2023-08-15 15:33:33 +0900

    custom auth manager used in filter

--- src/main/java/space/bum/config/filters/ApiKeyFilter.java
+++ src/main/java/space/bum/config/filters/ApiKeyFilter.java
@@ -8,22 +8,39 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import lombok.AllArgsConstructor;
+import space.bum.config.authentication.ApiKeyAuthentication;
+import space.bum.config.managers.CustomAuthenticationManager;
 
 // 이것을 어디에도 두고 싶진 않다.
 @AllArgsConstructor
-public class ApiKeyFilter extends OncePerRequestFilter{
+public class ApiKeyFilter extends OncePerRequestFilter {
 
 	@Value("${the.key}")
 	private final String key;
-	
+
 	@Override
 	protected void doFilterInternal(HttpServletRequest request,
 			HttpServletResponse response, FilterChain filterChain)
 			throws ServletException, IOException {
-		filterChain.doFilter(request, response); // 언젠가 호출해야...
+		var manager = new CustomAuthenticationManager();
+		var auth = new ApiKeyAuthentication(key);
+
+		try {
+			var a = manager.authenticate(auth);
+			if (a.isAuthenticated()) {
+				SecurityContextHolder.getContext().setAuthentication(a);
+				filterChain.doFilter(request, response); // 언젠가 호출해야...
+			} else {
+				response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+			}
+		} catch (AuthenticationException e) {
+			response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+		}
 	}
 
 }
